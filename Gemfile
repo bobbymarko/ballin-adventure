c8da4b0d2465dcc28dd44ae55f72e9f2f4e7b7f8
bobbymarko <bobby@bobby-marko.com>
2012-06-03 21:36:59 -0700

    testing basic service connection

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source :rubygems
 gem 'heroku'
 gem 'sinatra'
 gem 'thin'
+
+gem 'vacuum'
--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 
 get '/' do
-  "hello world"
+  req = Vacuum.new :product_advertising
+
+  req.configure do |config|
+    config.key    = ENV['amazon_key']
+    config.secret = ENV['amazon_secret']
+    config.tag    = 'wefouadv-20'
+  end
+  
+  req.build operation:    'ItemSearch',
+          search_index: 'Books',
+          keywords:     'Deleuze'
+  res = req.get
+  
+  res.valid? or raise res.code
+  
+  p res.body
+  
 end
